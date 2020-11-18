289671c30906ef8de087b531c06ba0d7b534600a
Angus Brooks <angusbrooks@gmail.com>
2020-11-18 08:38:04 -0500

    add paths to  db file sqlite3

--- py3x/bottleMarks/globals.py
+++ py3x/bottleMarks/globals.py
@@ -1,9 +1,7 @@
 tabMap = {'tab_AE':1,'tab_FJ':2,'tab_KP':3,'tab_QU':4,'tab_VZ':5,'tab_SRCH_TITLE':6,'tab_SRCH_URL':7,'tab_SRCH_DATE':8,'tab_DATE':9,'searchBox':10}
-connFile = gConn = webMarksDB = '/home/angus/dcoda_net/cgi-bin/webMarks/cgi-bin/dcoda_acme.webMarks'
+#connFile = gConn = webMarksDB = '/home/angus/dcoda_net/cgi-bin/webMarks/cgi-bin/dcoda_acme.webMarks'
+connFile = gConn = webMarksDB = '\\Users\\angus\\dcoda_net\\db\\dcoda_acme.webMarks'
+#connFile = gConn = webMarksDB = '\\Users\\angus\\pyproject\\db\\dcoda_acme.webMarks'
 #connFile = gConn =  webmarksDB = '/home/angus/pyprojects/webMarksMicro/bottleMarks/dcoda_acme.webMarks'
-conn_host = 'localhost'
-conn_dbdriver = 'mysql'
-conn_db = 'dcoda_acme'
-conn_user = 'dococt'
-conn_passwd = 'dococt'
+conn_host = 'localhost'; conn_dbdriver = 'mysql' ;conn_db = 'dcoda_acme' ;conn_user = 'dococt'; conn_passwd = 'dococt'
 
